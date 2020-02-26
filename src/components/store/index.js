06e5a45531220f405d13bd17d447cf5a17850e3f
Asura <aravind1808@gmail.com>
2020-02-26 13:08:49 +0530

    Saga

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,10 +1,11 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
+// import thunk from 'redux-thunk'
+import saga from 'redux-saga'
 import userReducer from '../views/Login/redux/reducer'
 const configureStore = () => {
     const store = createStore(combineReducers({
         user: userReducer
-    }),applyMiddleware(thunk))
+    }),applyMiddleware(saga))
     return store
 }
 export default configureStore
